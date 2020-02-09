11b3d712c97ba574625d2a36c4e99d1ef02d83ea
Ernest MARIN <emarin@student.42.fr>
2020-02-09 18:51:04 +0100

    better testing optionals name validity

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -18,47 +18,47 @@ int main(int ac, char * const *av) {
 	argsParser.setProgDescr("The purpose of this project is to create our version of the game Snake,\n"\
 	"with at least 3 different GUIs. These GUIs being shared libraries.");
 
-	argsParser.addArgument("testBool", ArgType::STRING);
-	argsParser.addArgument("testString", ArgType::BOOL);
-	argsParser.addArgument("testInt32", ArgType::INT32);
-	argsParser.addArgument("testInt64", ArgType::INT64);
-	argsParser.addArgument("testUint32", ArgType::UINT32);
-	argsParser.addArgument("testUint64", ArgType::UINT64);
-	argsParser.addArgument("testFloat", ArgType::FLOAT);
+	// argsParser.addArgument("testBool", ArgType::STRING);
+	// argsParser.addArgument("testString", ArgType::BOOL);
+	// argsParser.addArgument("testInt32", ArgType::INT32);
+	// argsParser.addArgument("testInt64", ArgType::INT64);
+	// argsParser.addArgument("testUint32", ArgType::UINT32);
+	// argsParser.addArgument("testUint64", ArgType::UINT64);
+	// argsParser.addArgument("testFloat", ArgType::FLOAT);
 
-	// // width
-	// argsParser.addArgument("width", ArgType::UINT32)
-	// 	.setHelp("set the windows width")
-	// 	.setMinUI32(200)
-	// 	.setMaxUI32(7680)
-	// 	.setDefaultUI32(1200);
+	// width
+	argsParser.addArgument("width", ArgType::UINT32)
+		.setHelp("set the windows width")
+		.setMinUI32(200)
+		.setMaxUI32(7680)
+		.setDefaultUI32(1200);
 
-	// // height
-	// argsParser.addArgument("height", ArgType::UINT32)
-	// 	.setHelp("set the windows height")
-	// 	.setMinUI32(200)
-	// 	.setMaxUI32(4320)
-	// 	.setDefaultUI32(800);
+	// height
+	argsParser.addArgument("height", ArgType::UINT32)
+		.setHelp("set the windows height")
+		.setMinUI32(200)
+		.setMaxUI32(4320)
+		.setDefaultUI32(800);
 
-	// // --gui -g
-	// argsParser.addArgument("gui", ArgType::UINT32)
-	// 	.setOptional("gui", 'g')
-	// 	.setHelp("change the starting gui id")
-	// 	.setMaxUI32(2)
-	// 	.setDefaultUI32(0);
+	// --gui -g
+	argsParser.addArgument("gui", ArgType::UINT32)
+		.setOptional("gui", 'g')
+		.setHelp("change the starting gui id")
+		.setMaxUI32(2)
+		.setDefaultUI32(0);
 
-	// // --boardSize -b
-	// argsParser.addArgument("boardSize", ArgType::UINT32)
-	// 	.setOptional("boardSize", 'b')
-	// 	.setHelp("set the board size")
-	// 	.setDefaultUI32(16);
-	// // --speed -s
-	// argsParser.addArgument("speed", ArgType::FLOAT)
-	// 	.setOptional("speed", 's')
-	// 	.setHelp("change the game speed")
-	// 	.setMinF(0.0f)
-	// 	.setMaxF(100.0f)
-	// 	.setDefaultF(10.5f);
+	// --boardSize -b
+	argsParser.addArgument("boardSize", ArgType::UINT32)
+		.setOptional("boardSize", 'b')
+		.setHelp("set the board size")
+		.setDefaultUI32(16);
+	// --speed -s
+	argsParser.addArgument("speed", ArgType::FLOAT)
+		.setOptional("speed", 's')
+		.setHelp("change the game speed")
+		.setMinF(0.0f)
+		.setMaxF(100.0f)
+		.setDefaultF(10.5f);
 
 	argsParser.parseArgs();
 
--- srcs/utils/ArgsParser/ArgsParser.cpp
+++ srcs/utils/ArgsParser/ArgsParser.cpp
@@ -108,22 +108,54 @@ void	ArgsParser::usage() const {
 	}
 }
 
+// test optionals name validity
 bool	ArgsParser::checkOptsAvailability(std::string const &longName, char shortName) {
-	// The -W option is reserved by POSIX.2 for implementation extensions
-	if (shortName == 'W') {
-		logErr("the -W option is reserved by POSIX.2 for implementation extensions");
+	std::string const	errInfo = "setOptional('" + std::string(1, shortName) + \
+		"', \"" + longName + "\"): ";
+
+	// refuse empty longName && empty shortName
+	if (longName.empty() && shortName == A_NO_NAME) {
+		logErr(errInfo << "you need to specify at least a shortName or a longName");
+		return EXIT_FAILURE;
+	}
+
+	// shortName invalid char
+	if (!isalnum(shortName) && shortName != A_NO_NAME) {
+		logErr(errInfo << "shortName only accept alphanumeric chars");
+		return EXIT_FAILURE;
+	}
+
+	// accept only alphanumeric and '-' '_' chars
+	auto isInvalidChar = [](char c) {
+		return !(isalnum(c) || c == ' ' || c == '-' || c == '_'); };
+	// longName invalid char
+	if (!longName.empty() && find_if(longName.begin(), longName.end(), isInvalidChar)
+		!= longName.end()) {
+		logErr(errInfo << "longName only accept alphanumeric, '-' and '_' chars");
 		return EXIT_FAILURE;
 	}
 
 	// refuse char '-' at the begining of a long/short name
 	if (shortName == '-' || longName[0] == '-') {
-		logErr("you can't put char '-' at the begining of a long/short name");
+		logErr(errInfo << "you can't put char '-' at the begining of a long/short name");
 		return EXIT_FAILURE;
 	}
 
-	// refuse empty longName && empty shortName
-	if (longName.empty() && shortName == A_NO_NAME) {
-		logErr("you need to specify at least a shortName or a longName");
+	// The -W option is reserved by POSIX.2 for implementation extensions
+	if (shortName == 'W') {
+		logErr(errInfo << "the -W option is reserved by POSIX.2 for implementation extensions");
+		return EXIT_FAILURE;
+	}
+
+	// The -u option is reserved for usage
+	if (shortName == 'u') {
+		logErr(errInfo << "The -u short option is reserved for usage");
+		return EXIT_FAILURE;
+	}
+
+	// The -u option is reserved for usage
+	if (longName == "usage") {
+		logErr(errInfo << "The --usage long option is reserved for usage");
 		return EXIT_FAILURE;
 	}
 
@@ -134,7 +166,7 @@ bool	ArgsParser::checkOptsAvailability(std::string const &longName, char shortNa
 				argInfos->getShortName() == shortName) || (!argInfos->getLongName().empty() && argInfos->getLongName() == longName);
 		});
 	if (nbDuplicates > 0) {
-		logErr("duplicate optional arg name: " << "-" << shortName << ", --" << longName);
+		logErr(errInfo << "duplicate optional arg name: " << "-" << shortName << ", --" << longName);
 		return EXIT_FAILURE;
 	}
 
@@ -191,6 +223,9 @@ AInfoArg	&ArgsParser::addArgument(std::string name, ArgType::Enum type) {
 }
 
 void	ArgsParser::init() {
+	_opts = 'u';
+	_longOpts = {{ "usage", no_argument, NULL, 'u' }};
+
 	// fill _opts string
 	for (auto &&argInfos : _argsInfos) {
 		if (!argInfos->getRequired()) {
@@ -223,42 +258,35 @@ void	ArgsParser::init() {
 // then option processing stops as soon as a nonoption argument is encountered
 
 void	ArgsParser::parseArgs() {
-	// int	opt;
-	// int	longIndex = 0;
-	(void)_ac;
+	int	opt;
+	int	longIndex = 0;
 
 	init();  // init getopt_long args
 
-	usage();
+	while ((opt = getopt_long(_ac, _av, _opts.c_str(), _longOpts.data(), &longIndex)) != -1) {
+		logDebug("opt: " << static_cast<char>(opt));
 
-	// while ((opt = getopt_long(_ac, _av, _opts.c_str(), _longOpts.data(), &longIndex)) != -1) {
-	// 	std::cout << "opt: " << static_cast<char>(opt) << std::endl;
-	// 	switch (opt) {
-	// 		case 0:
-	// 			std::cout << "long opt: " << _longOpts[longIndex].name << std::endl;
-	// 			if (optarg) {
-	// 				std::cout << "with arg \"" << optarg << "\"" << std::endl;
-	// 			}
-	// 			break;
-	// 		case 'w':
-	// 			std::cout << "optarg: " << optarg << std::endl;
-	// 			break;
-	// 		case 'g':
-	// 			std::cout << "optarg: " << optarg << std::endl;
-	// 			break;
-	// 		case 's':
-	// 			std::cout << "optarg: " << optarg << std::endl;
-	// 			break;
-	// 		case 'b':
-	// 			std::cout << "optarg: " << optarg << std::endl;
-	// 			break;
-	// 		case 'u': case '?': default:
-	// 			std::cout << "// usage();" << std::endl;
-	// 			usage();
-	// 			break;
-	// 		break;
-	// 	}
-	// }
+		// long option
+		if (opt == 0) {
+				std::cout << "long opt: " << _longOpts[longIndex].name << std::endl;
+				if (optarg) {
+					std::cout << "with arg \"" << optarg << "\"" << std::endl;
+				}
+		}
+		// show usage and stop
+		else if (opt == '?' || opt == 'u' || opt == ':') {
+			std::cout << "// usage();" << std::endl;
+			// usage();
+			break;
+		}
+		// short option
+		else {
+			std::cout << "short: '" << opt << "'" << std::endl;
+			if (true) {  // ? condition ?
+				std::cout << "optarg: " << optarg << std::endl;
+			}
+		}
+	}
 }
 
 void	ArgsParser::setProgDescr(std::string const &progDescr) { _progDescr = progDescr; }
